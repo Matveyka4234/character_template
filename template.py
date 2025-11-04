1cfc5c8066dec3fa6c6f5438d3df582e39299f13
Matveyka4234 <matvejtrub@gmail.com>
2025-11-04 20:52:55 +0500

    исправил код

--- template.py
+++ template.py
@@ -1,13 +1,8 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import random
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
 
-clases_base = {
+CLASSES_BASE = {
     "wizard" : {
         "strength" : random.randint(1,3),
         "agility" : random.randint(1,3),
@@ -45,38 +40,90 @@ clases_base = {
     }
 }
 
-skills = [
-    "Стремительный прыжок",
-    "Электрический выстрел",
-    "Ледяной удар",
-    "Стремительный удар",
-    "Кислотный взгляд",
-    "Тайный побег",
-    "Ледяной выстрел",
-    "Огненный заряд",
-]
+SKILLS = {
+    "wizard": [
+        "Огненный шар",
+        "Ледяная стрела",
+        "Электрический разряд",
+        "Магический щит",
+        "Телепортация",
+        "Молния небес",
+        "Кислотный туман",
+        "Психический удар"
+    ],
+    "warrior": [
+        "Мощный удар",
+        "Размашистый взмах",
+        "Бросок топора",
+        "Щитовой удар",
+        "Вихрь клинков",
+        "Пронзающий удар",
+        "Военный клич",
+        "Землетрясение"
+    ],
+    "hunter": [
+        "Меткий выстрел",
+        "Зов природы",
+        "Кровавая рана",
+        "Стальной капкан",
+        "Призыв ястреба",
+        "Дезориентирующий выстрел",
+        "Укус виверны",
+        "Маскировка в листве"
+    ],
+    "assassin": [
+        "Смертельный удар",
+        "Незаметное приближение",
+        "Ядовитый клинок",
+        "Теневой клинок",
+        "Внезапное исчезновение",
+        "Мгновенный бросок",
+        "Глушащий удар",
+        "Фантомный шаг"
+    ],
+    "bard": [
+        "Стремительный прыжок",
+        "Электрический выстрел",
+        "Ледяной удар",
+        "Стремительный удар",
+        "Кислотный взгляд",
+        "Тайный побег",
+        "Ледяной выстрел",
+        "Огненный заряд"
+    ]
+}
 
-skill_list = random.sample(skills, 3)
-
-question = int(input("Введите количество карточек: "))
-for i in range(question):
-    name = input("Введите имя: ")
-    race = input("Введите рассу: ")
-    character_class = input("Введите свой класс: ")
-    rendered_page = template.render(
-        image = f".\images\{character_class}.png",
-        name = name[i],
-        race = race[i],
-        character_class = character_class[i],
-        strength = clases_base[character_class]["strength"][i],
-        agility = clases_base[character_class]["agility"][i],
-        intelligence = clases_base[character_class]["intelligence"][i],
-        luck = clases_base[character_class]["luck"][i],
-        temper = clases_base[character_class]["temper"][i],
-        first_skill = skill_list[1][i],
-        second_skill = skill_list[2][i],
-        third_skill = skill_list[3][i]
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
     )
+    template = env.get_template('template.html')
 
-with open(f'.\characters\index_{i}.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    card_quantity = int(input("Введите количество карточек: "))
+    for i in range(card_quantity):
+        name = input("Введите имя: ")
+        race = input("Введите рассу: ")
+        character_class = input("Введите свой класс: ")
+        skill_list = random.sample(SKILLS[character_class], 3)
+        rendered_page = template.render(
+            image = f".\images\{character_class}.png",
+            name = name[i],
+            race = race[i],
+            character_class = character_class[i],
+            strength = CLASSES_BASE[character_class]["strength"][i],
+            agility = CLASSES_BASE[character_class]["agility"][i],
+            intelligence = CLASSES_BASE[character_class]["intelligence"][i],
+            luck = CLASSES_BASE[character_class]["luck"][i],
+            temper = CLASSES_BASE[character_class]["temper"][i],
+            first_skill = skill_list[0][i],
+            second_skill = skill_list[1][i],
+            third_skill = skill_list[2][i]
+        )
+
+    with open(f'.\characters\index_{i}.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+if __name__ == "__main__":
+    main()
